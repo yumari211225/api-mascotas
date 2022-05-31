999c59f1f449e349109cf753a881284cb22bc699
yumari <211225@ids.upchiapas.edu.mx>
2022-05-30 21:29:48 -0500

    first commit

---
+++ src/test/java/com/application/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
