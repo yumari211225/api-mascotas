820cba7030d5c373ba3e0f8862098295e059a1ab
yumari <211225@ids.upchiapas.edu.mx>
2022-05-30 22:15:11 -0500

    Pet Model, Conctroller and Response implemented

---
+++ src/main/java/com/application/Controllers/PetsController.java
@@ -0,0 +1,13 @@
+package com.application.Controllers;
+
+import com.application.dto.response.PetResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pet")
+public class PetsController {
+}
---
+++ src/main/java/com/application/Models/Pet.java
@@ -0,0 +1,57 @@
+package com.application.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="mascotas")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String kind;
+    private int age;
+    private String gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
